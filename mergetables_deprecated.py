be1117b278be55278504f4b4719b26e8109d2958
Rohit Chatterjee <rohit@rootbridgeservices.in>
2023-10-13 21:45:13 +0530

    mergetables uses only dbt_utlils no premerge* step

--- mergetables_deprecated.py
+++ mergetables_deprecated.py
@@ -9,7 +9,8 @@ from dotenv import load_dotenv
 import yaml
 
 from lib.dbtproject import dbtProject
-
+from lib.dbtconfigs import get_columns_from_model
+from lib.postgres import fmt_colname
 
 basicConfig(level=INFO)
 logger = getLogger()
@@ -17,11 +18,14 @@ logger = getLogger()
 
 # ================================================================================
 parser = argparse.ArgumentParser()
+parser.add_argument("--models", required=True, help="models.yml")
 parser.add_argument("--mergespec", required=True)
+parser.add_argument("--warehouse", choices=["postgres", "bigquery"], required=True)
 args = parser.parse_args()
 
 # ================================================================================
 load_dotenv("dbconnection.env")
+
 project_dir = os.getenv("DBT_PROJECT_DIR")
 
 # ================================================================================
@@ -29,8 +33,7 @@ with open(args.mergespec, "r", encoding="utf-8") as mergespecfile:
     mergespec = yaml.safe_load(mergespecfile)
 
 table_counts = Counter([m["tablename"] for m in mergespec["tables"]])
-# pylint:disable=invalid-name
-has_error = False
+has_error: bool = False
 for tablename, tablenamecount in table_counts.items():
     if tablenamecount > 1:
         logger.error("table appears more than once in spec: %s", tablename)
@@ -41,10 +44,35 @@ if has_error:
 dbtproject = dbtProject(project_dir)
 dbtproject.ensure_models_dir(mergespec["outputsschema"])
 
+with open(args.models, "r", encoding="utf-8") as modelfile:
+    models = yaml.safe_load(modelfile)
+
+all_columns = set()
+for table in mergespec["tables"]:
+    columns = get_columns_from_model(models, table["tablename"])
+    all_columns = all_columns.union(columns)
+
+for table in mergespec["tables"]:
+    columns = get_columns_from_model(models, table["tablename"])
+    statement: str = "{{ config(materialized='table',) }}\nSELECT "
+    for column in all_columns:
+        if column in columns:
+            statement += f"{fmt_colname(column)} AS {fmt_colname(column)},"
+        else:
+            statement += f"NULL AS {fmt_colname(column)},"
+    statement = statement[:-1]  # drop the final comma
+    statement += f" FROM {{{{ref('{table['tablename']}')}}}}"
+    dbtproject.write_model(
+        mergespec["outputsschema"],
+        f'premerge_{table["tablename"]}',
+        statement,
+        subdir="premerge",
+    )
+
 # pylint:disable=invalid-name
 relations = "["
 for table in mergespec["tables"]:
-    relations += f"ref('{table['tablename']}'),"
+    relations += f"ref('premerge_{table['tablename']}'),"
 relations = relations[:-1]
 relations += "]"
 union_code = "{{ config(materialized='table',) }}\n"
@@ -54,6 +82,6 @@ union_code += f"relations={relations}"
 union_code += ")}}"
 dbtproject.write_model(
     mergespec["outputsschema"],
-    mergespec["mergename"],
+    f"merged_{mergespec['mergename']}",
     union_code,
 )
