372078ba2d9e93e50415af0235a22c1f55bdcb1b
Ishan <koradiaishan335@gmail.com>
2023-10-24 14:07:44 +0530

    minor change

--- dbt_automation/utils/warehouseclient.py
+++ dbt_automation/utils/warehouseclient.py
@@ -4,10 +4,10 @@ from dbt_automation.utils.postgres import PostgresClient
 from dbt_automation.utils.bigquery import BigQueryClient
 
 
-def get_client(warehouse: str):
+def get_client(warehouse: str, conn_info: dict = None):
     """constructs and returns an instance of the client for the right warehouse"""
     if warehouse == "postgres":
-        client = PostgresClient()
+        client = PostgresClient(conn_info)
     elif warehouse == "bigquery":
         client = BigQueryClient()
     else:
