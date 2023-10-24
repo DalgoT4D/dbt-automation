226ad001b28097c89d8ad23a39f5d135b6c6d393
Ishan <koradiaishan335@gmail.com>
2023-10-24 11:28:08 +0530

    refactoring and adding necessary package info

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup, find_packages
 
+long_description = ""
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="dbt_automation",
     packages=find_packages(),
@@ -9,4 +13,12 @@ setup(
     description="This package helps you automate your transformation by generating dbt code",
     license="MIT",
     url="https://github.com/DalgoT4D/dbt-automation",
+    project_urls={
+        "Documentation": "https://github.com/DalgoT4D/dbt-automation",
+        "Source": "https://github.com/DalgoT4D/dbt-automation",
+        "Tracker": "https://github.com/DalgoT4D/dbt-automation/issues",
+    },
+    long_description=long_description,
+    python_requires=">=3.7",
+    install_requires=["PyYAML", "requests", "google-cloud-bigquery", "psycopg2-binary"],
 )
