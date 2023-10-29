b15e86b7b68ef637f9c0b5ce449ab6fc3f7cae7f
Rohit K. Chatterjee <fatchat@users.noreply.github.com>
2023-10-29 15:50:43 +0530

    Update dbt_automation/operations/flattenjson.py
    Co-authored-by: Ishankoradia <39583356+Ishankoradia@users.noreply.github.com>

--- dbt_automation/operations/flattenjson.py
+++ dbt_automation/operations/flattenjson.py
@@ -34,7 +34,7 @@ def flattenjson(config: dict, warehouse, project_dir: str):
     if columns_to_copy == "*":
         model_code += "SELECT *\n"
     else:
-        model_code += f"SELECT {', '.join(columns_to_copy)}\n"
+        model_code += f"SELECT {', '.join([quote_columnname(col, warehouse.name) for col in columns_to_copy])}\n"
 
     # json_columns = warehouse.get_json_columnspec(source_schema, input_name, json_column)
 
