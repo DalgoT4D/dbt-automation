ceb861ea122c3542a53484e6fc89f7548d04e3b2
Ishan <koradiaishan335@gmail.com>
2023-11-03 11:55:19 +0530

    seeder finished - to seed only airbyte raw data - using truncate instead of deleting

--- seeds/seed.py
+++ seeds/seed.py
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 import csv
 from pathlib import Path
 import json
+from google.cloud import bigquery
 
 
 basicConfig(level=INFO)
@@ -49,9 +50,6 @@ if args.warehouse == "postgres":
 
     wc_client = get_client(args.warehouse, conn_info)
 
-    drop_schema_query = f"""
-        DROP SCHEMA IF EXISTS {schema} CASCADE;
-    """
     create_schema_query = f"""
         CREATE SCHEMA IF NOT EXISTS {schema};
     """
@@ -63,10 +61,13 @@ if args.warehouse == "postgres":
             _airbyte_emitted_at timestamp with time zone
         );
     """
+    truncate_table_query = f"""
+        TRUNCATE TABLE {schema}."{tablename}";
+    """
 
-    wc_client.runcmd(drop_schema_query)
     wc_client.runcmd(create_schema_query)
     wc_client.runcmd(create_table_query)
+    wc_client.runcmd(truncate_table_query)
 
     """
     INSERT INTO your_table_name (column1, column2, column3, ...)
@@ -79,12 +80,58 @@ if args.warehouse == "postgres":
         insert_query = f"""INSERT INTO {schema}."{tablename}" ({', '.join(columns)}) VALUES ('{row['_airbyte_ab_id']}', JSON '{row['_airbyte_data']}', '{row['_airbyte_emitted_at']}')"""
         wc_client.runcmd(insert_query)
 
-    wc_client.close()
-    logger.info("seeding finished")
-
 
 if args.warehouse == "bigquery":
     logger.info("Found bigquery warehouse")
-    conn_info = os.getenv("TEST_BG_SERVICEJSON")
-    print(conn_info)
-    print(type(conn_info))
+    conn_info = json.loads(os.getenv("TEST_BG_SERVICEJSON"))
+    location = os.getenv("TEST_BG_LOCATION")
+    test_dataset = os.getenv("TEST_BG_DATASET")
+
+    wc_client = get_client(args.warehouse, conn_info)
+
+    # create the dataset if it does not exist
+    dataset = bigquery.Dataset(f"{conn_info['project_id']}.{test_dataset}")
+    dataset.location = location
+
+    logger.info("creating the dataset")
+    dataset = wc_client.bqclient.create_dataset(dataset, timeout=30, exists_ok=True)
+    logger.info("created dataset : {}".format(dataset.dataset_id))
+
+    # create the staging table if its does not exist
+    table_schema = [
+        bigquery.SchemaField("_airbyte_ab_id", "STRING", mode="REQUIRED"),
+        bigquery.SchemaField("_airbyte_data", "STRING", mode="REQUIRED"),
+        bigquery.SchemaField("_airbyte_emitted_at", "TIMESTAMP", mode="REQUIRED"),
+    ]
+    table = bigquery.Table(
+        f"{conn_info['project_id']}.{dataset.dataset_id}.{tablename}",
+        schema=table_schema,
+    )
+    wc_client.bqclient.create_table(table, exists_ok=True)
+
+    # truncate data to (re-)seed fresh data
+    truncate_table_query = f"""
+        DELETE FROM `{conn_info['project_id']}.{dataset.dataset_id}.{tablename}`
+        WHERE true;
+    """
+
+    wc_client.execute(truncate_table_query)
+
+    # seed data
+    insert_query = f"""
+    INSERT INTO `{conn_info['project_id']}.{dataset.dataset_id}.{tablename}` (_airbyte_ab_id, _airbyte_data, _airbyte_emitted_at)
+    VALUES 
+    """
+    insert_query_values = ",".join(
+        [
+            f"""('{row["_airbyte_ab_id"]}', '{row["_airbyte_data"]}', '{row["_airbyte_emitted_at"]}')"""
+            for row in data
+        ]
+    )
+    insert_query += insert_query_values
+
+    wc_client.execute(insert_query)
+
+
+wc_client.close()
+logger.info("seeding finished")
