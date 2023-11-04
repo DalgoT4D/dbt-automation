e393faedff37f2a8123f126a2fb4ade865f81074
Ishan <koradiaishan335@gmail.com>
2023-11-04 12:45:34 +0530

    drop and rename cols test cases

--- tests/warehouse/postgres/test_operations.py
+++ tests/warehouse/postgres/test_operations.py
@@ -3,7 +3,7 @@ import os
 from pathlib import Path
 import subprocess, sys
 from logging import basicConfig, getLogger, INFO
-from dbt_automation.operations.droprenamecolumns import rename_columns
+from dbt_automation.operations.droprenamecolumns import rename_columns, drop_columns
 from dbt_automation.utils.warehouseclient import get_client
 from dbt_automation.operations.scaffold import scaffold
 from dbt_automation.operations.syncsources import sync_sources
@@ -33,9 +33,26 @@ class TestPostgresOperations:
         "TEST_PG_DBSCHEMA_SRC"
     )  # source schema where the raw data lies
 
+    @staticmethod
+    def execute_dbt(cmd: str, select_model: str = None):
+        select_cli = ["--select", select_model] if select_model is not None else []
+        subprocess.call(
+            [
+                Path(TestPostgresOperations.test_project_dir) / "venv" / "bin" / "dbt",
+                cmd,
+            ]
+            + select_cli
+            + [
+                "--project-dir",
+                TestPostgresOperations.test_project_dir,
+                "--profiles-dir",
+                TestPostgresOperations.test_project_dir,
+            ],
+        )
+
     def test_scaffold(self, tmpdir):
         """This will setup the dbt repo to run dbt commands after running a test operation"""
-        print("starting scaffolding")
+        logger.info("starting scaffolding")
         project_name = "pytest_dbt"
         config = {
             "project_name": project_name,
@@ -43,21 +60,12 @@ class TestPostgresOperations:
         }
         scaffold(config, TestPostgresOperations.wc_client, tmpdir)
         TestPostgresOperations.test_project_dir = Path(tmpdir) / project_name
-        subprocess.call(
-            [
-                Path(TestPostgresOperations.test_project_dir) / "venv" / "bin" / "dbt",
-                "deps",
-                "--project-dir",
-                TestPostgresOperations.test_project_dir,
-                "--profiles-dir",
-                TestPostgresOperations.test_project_dir,
-            ],
-        )
-        print("finished scaffolding")
+        TestPostgresOperations.execute_dbt("deps")
+        logger.info("finished scaffolding")
 
     def test_syncsources(self):
         """test the sync sources operation against the warehouse"""
-        print("syncing sources")
+        logger.info("syncing sources")
         config = {
             "source_name": "sample",
             "source_schema": TestPostgresOperations.schema,
@@ -74,51 +82,73 @@ class TestPostgresOperations:
             / "sources.yml"
         )
         assert os.path.exists(sources_yml) is True
-        print("finished syncing sources")
+        logger.info("finished syncing sources")
 
     def test_flatten(self):
         """test the flatten operation against the warehouse"""
+        wc_client = TestPostgresOperations.wc_client
         config = {
             "source_schema": TestPostgresOperations.schema,
             "dest_schema": "pytest_intermediate",
         }
         flatten_operation(
             config,
-            TestPostgresOperations.wc_client,
+            wc_client,
             TestPostgresOperations.test_project_dir,
         )
-        subprocess.call(
-            [
-                Path(TestPostgresOperations.test_project_dir) / "venv" / "bin" / "dbt",
-                "run",
-                "--project-dir",
-                TestPostgresOperations.test_project_dir,
-                "--profiles-dir",
-                TestPostgresOperations.test_project_dir,
-            ],
+        TestPostgresOperations.execute_dbt("run", "Sheet1")
+        logger.info("inside test flatten")
+        logger.info(
+            f"inside project directory : {TestPostgresOperations.test_project_dir}"
         )
-        print("inside test flatten")
-        print(f"inside project directory : {TestPostgresOperations.test_project_dir}")
         assert "Sheet1" in TestPostgresOperations.wc_client.get_tables(
             "pytest_intermediate"
         )
 
-    # def test_rename_columns(self):
-    #     """test rename_columns for sample seed data"""
-    #     config = {"input_name": "", "dest_schema": "", "output_name": "", "columns": {}}
+    def test_rename_columns(self):
+        """test rename_columns for sample seed data"""
+        wc_client = TestPostgresOperations.wc_client
+        output_name = "rename"
+        config = {
+            "input_name": "Sheet1",
+            "dest_schema": "pytest_intermediate",
+            "output_name": output_name,
+            "columns": {"NGO": "ngo", "Month": "month"},
+        }
 
-    #     rename_columns(
-    #         config,
-    #         TestPostgresOperations.wc_client,
-    #         TestPostgresOperations.test_project_dir,
-    #     )
+        rename_columns(
+            config,
+            wc_client,
+            TestPostgresOperations.test_project_dir,
+        )
 
-    #     subprocess.call(
-    #         [
-    #             "cd",
-    #             self.test_project_dir,
-    #             "&&",
-    #             Path(self.test_project_dir) / "venv" / "bin" / "dbt",
-    #             "debug",
-    #         ],
-    #     )
+        TestPostgresOperations.execute_dbt("run", output_name)
+
+        cols = wc_client.get_table_columns("pytest_intermediate", output_name)
+        assert "ngo" in cols
+        assert "month" in cols
+        assert "NGO" not in cols
+        assert "MONTH" not in cols
+
+    def test_drop_columns(self):
+        """test drop_columns"""
+        wc_client = TestPostgresOperations.wc_client
+        output_name = "drop"
+
+        config = {
+            "input_name": "Sheet1",
+            "dest_schema": "pytest_intermediate",
+            "output_name": output_name,
+            "columns": ["MONTH"],
+        }
+
+        drop_columns(
+            config,
+            wc_client,
+            TestPostgresOperations.test_project_dir,
+        )
+
+        TestPostgresOperations.execute_dbt("run", output_name)
+
+        cols = wc_client.get_table_columns("pytest_intermediate", output_name)
+        assert "MONTH" not in cols
