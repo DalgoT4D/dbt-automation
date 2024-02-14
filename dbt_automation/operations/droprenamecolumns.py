cda097edeefd98a2246c13416ed200eefb168e59
Ishankoradia <ikoradia@umich.edu>
2024-02-14 11:31:54 +0530

    making code readable and allow drop column op to accept source or model

--- dbt_automation/operations/droprenamecolumns.py
+++ dbt_automation/operations/droprenamecolumns.py
@@ -5,7 +5,7 @@ from logging import basicConfig, getLogger, INFO
 from dbt_automation.utils.dbtproject import dbtProject
 from dbt_automation.utils.columnutils import quote_columnname
 from dbt_automation.utils.interfaces.warehouse_interface import WarehouseInterface
-
+from dbt_automation.utils.tableutils import source_or_ref
 
 basicConfig(level=INFO)
 logger = getLogger()
@@ -15,13 +15,18 @@ logger = getLogger()
 def drop_columns(config: dict, warehouse: WarehouseInterface, project_dir: str):
     """drops columns from a model"""
     dest_schema = config["dest_schema"]
-    input_name = config["input_name"]
     columns = config.get("columns", [])
     output_model_name = config["output_name"]
 
     model_code = f'{{{{ config(materialized="table", schema="{dest_schema}") }}}}\n'
-    columns = ",".join([f'"{col}"' for col in columns])
-    model_code += f'SELECT {{{{ dbt_utils.star(from=ref("{input_name}"), except=[{columns}]) }}}} FROM {{{{ref("{input_name}")}}}}\n'
+    model_code += (
+        "SELECT {{dbt_utils.star(from="
+        + source_or_ref(**config["input"])
+        + ", except=["
+    )
+    model_code += ",".join([f'"{col}"' for col in columns])
+    model_code += "])}}"
+    model_code += " FROM " + "{{" + source_or_ref(**config["input"]) + "}}" + "\n"
 
     dbtproject = dbtProject(project_dir)
     dbtproject.ensure_models_dir(dest_schema)
