234fa3f4e54186e282cdd054c9dcf7da9b0ac92e
Ishankoradia <ikoradia@umich.edu>
2024-02-14 11:37:11 +0530

    rename cols to accept source or model as input

--- dbt_automation/operations/droprenamecolumns.py
+++ dbt_automation/operations/droprenamecolumns.py
@@ -36,7 +36,7 @@ def drop_columns(config: dict, warehouse: WarehouseInterface, project_dir: str):
 
 def rename_columns(config: dict, warehouse, project_dir: str):
     """renames columns in a model"""
-    input_name = config["input_name"]
+    # input_name = config["input_name"]
     dest_schema = config["dest_schema"]
     columns = config.get("columns", {})
     output_model_name = config["output_name"]
@@ -45,15 +45,19 @@ def rename_columns(config: dict, warehouse, project_dir: str):
     dbtproject.ensure_models_dir(dest_schema)
 
     model_code = '{{ config(materialized="table", schema="' + dest_schema + '") }}\n\n'
-    exclude_cols = ",".join([f'"{col}"' for col in columns.keys()])
-    model_code += f'SELECT {{{{ dbt_utils.star(from=ref("{input_name}"), except=[{exclude_cols}]) }}}}, '
+    model_code += (
+        "SELECT {{dbt_utils.star(from="
+        + source_or_ref(**config["input"])
+        + ", except=["
+    )
+    model_code += ",".join([f'"{col}"' for col in columns.keys()])
+    model_code += "])}} , "
 
     for old_name, new_name in columns.items():
-        model_code += f"""{quote_columnname(old_name, warehouse.name)}
-                    AS {quote_columnname(new_name, warehouse.name)}, """
+        model_code += f"""{quote_columnname(old_name, warehouse.name)} AS {quote_columnname(new_name, warehouse.name)}, """
 
     model_code = model_code[:-2]  # Remove trailing comma and space
-    model_code += f'\nFROM \n  {{{{ ref("{input_name}") }}}}'
+    model_code += " FROM " + "{{" + source_or_ref(**config["input"]) + "}}" + "\n"
 
     model_sql_path = dbtproject.write_model(dest_schema, output_model_name, model_code)
     return model_sql_path
