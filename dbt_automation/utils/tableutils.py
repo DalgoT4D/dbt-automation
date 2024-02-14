fc94bc8ddcbb2fa3b9e5269f67f6ee9f61591797
Ishankoradia <ikoradia@umich.edu>
2024-02-14 11:47:03 +0530

    utility function for source or ref

---
+++ dbt_automation/utils/tableutils.py
@@ -0,0 +1,15 @@
+"""
+This file will take all helpers need to work with dbt sources and dbt models
+"""
+
+
+def source_or_ref(source_name: str, input_name: str, input_type: str) -> str:
+    if input_type not in ["source", "model"]:
+        raise ValueError("invalid input type to select from")
+
+    source_or_ref = f"ref('{input_name}')"
+
+    if input_type == "source":
+        source_or_ref = f"source('{source_name}', '{input_name}')"
+
+    return source_or_ref
