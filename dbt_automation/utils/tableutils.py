f808ef14a16b44206f44ac4a6ac384ca3cd0dcbd
Abhishek-N <abhisheknshetty97@gmail.com>
2024-02-16 18:46:37 +0530

    add cte to source or ref

--- dbt_automation/utils/tableutils.py
+++ dbt_automation/utils/tableutils.py
@@ -4,8 +4,11 @@ This file will take all helpers need to work with dbt sources and dbt models
 
 
 def source_or_ref(source_name: str, input_name: str, input_type: str) -> str:
-    if input_type not in ["source", "model"]:
+    if input_type not in ["source", "model", "cte"]:
         raise ValueError("invalid input type to select from")
+
+    if input_type == "cte":
+        return input_name
 
     source_or_ref = f"ref('{input_name}')"
 
