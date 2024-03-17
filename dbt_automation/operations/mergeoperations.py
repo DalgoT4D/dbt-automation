f66095bf7ac8cb325f60e3ddd5551ad22319a0ea
Ishankoradia <ikoradia@umich.edu>
2024-03-17 14:43:04 +0530

    aggregate operations for column

--- dbt_automation/operations/mergeoperations.py
+++ dbt_automation/operations/mergeoperations.py
@@ -18,6 +18,7 @@ from dbt_automation.operations.replace import replace_dbt_sql
 from dbt_automation.operations.joins import joins_sql
 from dbt_automation.operations.wherefilter import where_filter_sql
 from dbt_automation.operations.groupby import groupby_dbt_sql
+from dbt_automation.operations.aggregate import aggregate_dbt_sql
 
 
 def merge_operations_sql(
@@ -99,6 +100,10 @@ def merge_operations_sql(
             )
         elif operation["type"] == "groupby":
             op_select_statement, out_cols = groupby_dbt_sql(
+                operation["config"], warehouse
+            )
+        elif operation["type"] == "aggregate":
+            op_select_statement, out_cols = aggregate_dbt_sql(
                 operation["config"], warehouse
             )
 
